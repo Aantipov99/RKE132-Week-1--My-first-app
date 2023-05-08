5e5be72c9834015de42db50f0ffa74ddad24bab2
Anna Antipova <aantipov@tktk.ee>
2023-05-08 13:16:15 +0300

    create new app

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+//rakendus küsib kasutajal nime
+//rakendus tervitab kasutajat nimepidi 
+
+Console.WriteLine("Enter your nime"); //Output
+//string-sõne
+string userName;
+userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+userName = Console.ReadLine();
+//string Interpolation
+Console.WriteLine($"Hello, (userName);!");
